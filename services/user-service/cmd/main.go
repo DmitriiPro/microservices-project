f9ce97c9a949787731f9c03ca290e0223c383995
Дмитрий <cf.gwod@gmail.com>
2026-01-27 17:40:10 +0300

    feature/services/user-service: добавил подключение в файл cmd/main.go

--- services/user-service/cmd/main.go
+++ services/user-service/cmd/main.go
@@ -5,14 +5,32 @@ import (
 	"log"
 	"net"
 
+	"github.com/DmitriiPro/user-service/internal/cache"
+	"github.com/DmitriiPro/user-service/internal/db"
 	"github.com/DmitriiPro/user-service/internal/handler"
 	userv1 "github.com/DmitriiPro/user-service/internal/pb/user"
+	"github.com/DmitriiPro/user-service/internal/repository"
+	"github.com/DmitriiPro/user-service/internal/service"
 	"google.golang.org/grpc"
 )
 
 var GrpcServerPORT = 50051
 
 func main() {
+
+	// db postgres 
+	dbConn := db.NewPostgres("postgres://postgres:password@localhost:5432/userdb?sslmode=disable")
+	defer dbConn.Close()
+
+	// db redis 
+	redisClient := cache.NewRedis("localhost:6379")
+	defer redisClient.Close()
+
+	repo := repository.NewUserRepository(dbConn)
+	service := service.NewUserService(repo, redisClient)
+	handler := handler.NewUserHandler(service)
+
+
 	//* starting gRPC server
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", GrpcServerPORT))
 
