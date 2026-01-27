d71a99405340059106781d2a0d81f0275c97c2fa
Дмитрий <cf.gwod@gmail.com>
2026-01-27 16:18:55 +0300

    feature/services/user-service: добавил в redis.go изменения

--- services/user-service/internal/config/redis.go
+++ services/user-service/internal/config/redis.go
@@ -4,10 +4,15 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+const (
+	RedisPort = "localhost:6379"
+)
 
 func NewRedisClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr:     RedisPort,
+		Password: "", // no password set
+		DB:       0,	// use default DB
 	})
 	return rdb
 }
