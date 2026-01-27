1c38106dfa9956a3b0cb18ed5ec7fa7cee5a12a4
Дмитрий <cf.gwod@gmail.com>
2026-01-27 16:30:56 +0300

    feature/services/user-service: cmd/main.go добавил небольшие улучшения

--- services/user-service/cmd/main.go
+++ services/user-service/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
@@ -9,19 +10,21 @@ import (
 	"google.golang.org/grpc"
 )
 
-var PORT = ":50051"
+var GrpcServerPORT = 50051
 
 func main() {
-	lis, err := net.Listen("tcp", PORT)
+	//* starting gRPC server
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", GrpcServerPORT))
+
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen starting gRPC server: %v", err)
 	}
 
 	s := grpc.NewServer()
 
 	userv1.RegisterUserServiceServer(s, handler.NewUserHandler())
 
-	log.Printf("User service is running on %v\n", PORT)
+	log.Printf("User service is running on %v\n", fmt.Sprintf("localhost:%d", GrpcServerPORT))
 	
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
